55a882047d8b60e31e508ac42ae5d7cc8eceba99
NamJihyun99 <namjihyun9936@gmail.com>
2022-08-28 16:53:42 +0900

    fix:> fix Dto Duplicate Bug/#5

--- src/main/java/com/team20/t4/plan/PlanController.java
+++ src/main/java/com/team20/t4/plan/PlanController.java
@@ -1,7 +1,6 @@
 package com.team20.t4.plan;
 
 import com.team20.t4.plan.domain.Progress;
-import com.team20.t4.plan.domain.ProgressUpdateDto;
 import com.team20.t4.plan.domain.State;
 import com.team20.t4.plan.dto.*;
 import lombok.RequiredArgsConstructor;
@@ -29,15 +28,11 @@ public class PlanController {
     /**아래 메소드 지우고 Post 수정할 때 PlanService.updatePlan() 같이 호출해도 됨*/
     @PostMapping("/plan/{planId}")
     public Long updatePlan(@PathVariable Long planId, @RequestBody PlanUpdateRequestDto dto) {
-        return planService.updatePlan(planId, dto).getId();
+        return planService.updatePlan(planId, dto);
     }
 
     @PostMapping("/plan/progress/{planId}")
-<<<<<<< HEAD
     public Long updateProgress(@PathVariable Long planId, @RequestBody UpdateProgressDto progress) {
-=======
-    public Long updateProgress(@PathVariable Long planId, @RequestBody ProgressUpdateDto progress) {
->>>>>>> 2ace5f0fbd8a28baa9d6ac7f06a870029caa93b9
         return planService.updateProgressState(planId, progress.getProgress());
     }
 
@@ -47,11 +42,7 @@ public class PlanController {
     }
 
     @PostMapping("/registerHistory/state/{registerHistoryId}")
-<<<<<<< HEAD
     public Long updateState(@PathVariable Long registerHistoryId, @RequestBody UpdateStateDto state) {
-=======
-    public Long updateState(@PathVariable Long registerHistoryId, @RequestBody StateUpdateDto state) {
->>>>>>> 2ace5f0fbd8a28baa9d6ac7f06a870029caa93b9
         return planService.response(registerHistoryId, state.getState());
     }
 }
